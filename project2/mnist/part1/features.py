982b6a2eecac0d262cbb5a04b90f37584923c184
Gabriel Tem Pass <41839984+gabrieltempass@users.noreply.github.com>
2022-06-29 09:05:29 -0300

    feat: Project onto principal components

--- project2/mnist/part1/features.py
+++ project2/mnist/part1/features.py
@@ -8,15 +8,13 @@ def project_onto_PC(X, pcs, n_components, feature_means):
     this function returns a new data array in which each sample in X
     has been projected onto the first n_components principal components.
     """
-    # TODO: first center data using the feature_means
-    # TODO: Return the projection of the centered dataset
-    #       on the first n_components principal components.
-    #       This should be an array with dimensions: n x n_components.
-    # Hint: these principal components = first n_components columns
-    #       of the eigenvectors returned by principal_components().
-    #       Note that each eigenvector is already be a unit-vector,
-    #       so the projection may be done using matrix multiplication.
-    raise NotImplementedError
+    # Center our data
+    X_centered = center_data(X)
+    # Chop the eigenvector matrix to pick the top 'n' eigenvectors
+    V_n = pcs[:, 0:n_components]
+    # Project the data onto the principal components
+    projected_data = X_centered @ V_n
+    return projected_data
 
 
 # Functions which are already complete, for you to use
